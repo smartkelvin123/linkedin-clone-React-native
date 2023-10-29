58a01ed0235847ba26fd1f7d6d4896e952bb1da3
unknown <smartkelvin66@gmail.com>
2023-10-29 11:46:52 +0100

    image picker

--- src/app/(tabs)/new-post.tsx
+++ src/app/(tabs)/new-post.tsx
@@ -1,11 +1,22 @@
 import { useNavigation, useRouter } from "expo-router";
 import { useLayoutEffect, useState } from "react";
-import { StyleSheet, TextInput, View, Pressable, Text } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Pressable,
+  Text,
+  Image,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { EvilIcons } from "@expo/vector-icons";
 
 export default function postScreen() {
   // binding the state variable to the state input
   const [content, setContent] = useState("");
-  const router = useRouter;
+  const [image, setImage] = useState<String | null>(null);
+
+  const router = useRouter();
 
   const navigation = useNavigation();
 
@@ -13,6 +24,7 @@ export default function postScreen() {
     console.warn("posting", content);
     router.push("/(tabs)/");
     setContent("");
+    setImage(null);
   };
 
   useLayoutEffect(() => {
@@ -25,6 +37,22 @@ export default function postScreen() {
     });
   }, [onPost]);
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -34,6 +62,25 @@ export default function postScreen() {
         style={styles.input}
         multiline
       />
+
+      {image && (
+        <Image
+          source={{ uri: image }}
+          style={{ width: 200, height: 200, aspectRatio: 1, marginTop: "auto" }}
+        />
+      )}
+
+      <View style={styles.footer}>
+        <Pressable style={styles.iconButton} onPress={pickImage}>
+          <EvilIcons name="image" size={24} color="grey" />
+        </Pressable>
+        <View style={styles.iconButton}>
+          <EvilIcons name="user" size={24} color="grey" />
+        </View>
+        <View style={styles.iconButton}>
+          <EvilIcons name="star" size={24} color="grey" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -65,5 +112,16 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     paddingHorizontal: 15,
     marginRight: 10,
+  },
+  footer: {
+    // alignSelf: "flex-end",
+    marginTop: "auto",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  iconButton: {
+    backgroundColor: "gainboro",
+    padding: 20,
+    borderRadius: 100,
   },
 });
